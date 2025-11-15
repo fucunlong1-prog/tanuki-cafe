9dc9daf498da36cfe52116d8ec35a4254b90cde7
たぬき <fucunlong1@gmail.com>
2025-11-15 18:01:14 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuIcon = document.querySelector(`.menu-icon`);
+const headerRight = document.querySelector(`.header-right`);
+
+menuIcon.addEventListener(`click`, () => {
+    headerRight.classList.toggle(`active`);
+});
